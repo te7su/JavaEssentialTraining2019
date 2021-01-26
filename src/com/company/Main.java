94267cd4b1d5efcba1ac9e644aa38141b4e7bade
Joe Chung <joec.jp2@gmail.com>
2021-01-26 18:56:12 -0500

    A simple calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var scan = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var f1 = scan.nextDouble();
+
+        System.out.print("Enter a numeric value: ");
+        var f2 = scan.nextDouble();
+
+        double res = f1 / f2;
+
+        System.out.println("The answer is " + res);
     }
 }
